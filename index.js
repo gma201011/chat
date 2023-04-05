fab3a8f207f22f2025737e61e960d4d4de5de42f
gma201011 <gma201011@hotmail.com>
2023-04-05 09:56:12 +0800

    Add db

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const axios = require("axios");
+
 const fs = require("fs");
 const data = require("./db");
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+// var db;
+
+// fs.readFileSync("db.json", (err, data) => {
+//   db = data;
+//   console.log('888888', data);
+// })
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    // console.log(data);
-    // const data = data;
+app.get("/db", (req, res) => {
+  res.sendFile(__dirname + "/db.json");
+});
 
-    // console.log(data);
-    // fs.writeFileSync("./db.js", data[, options], callback)
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    const dbcontent = data;
+    const msgObject = {
+      msg: {
+        content: msg,
+        time: Date.now(),
+      },
+    };
+    dbcontent.push(msgObject);
+
+    fs.writeFileSync("db.json", JSON.stringify(dbcontent), (err) => {
+      if (err) return console.log("Write file error");
+    });
+    io.emit("chat message", msg);
   });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
